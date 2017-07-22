BAR: error corrected (minus sign), now two versions, the new one for delta_U values as input

hqf/hqf_fec_run_bar_ip.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import division
 import sys
 import numpy as np
 import scipy
@@ -8,7 +9,7 @@ import matplotlib.pyplot as plt
 iteration = 0
 
 def bar_ip(delta_U1_filename, delta_U2_filename):
-    
+    # TODO: compute histograms, max overlap like GK, and if required abort. max overlap by min of of both historgrams, then max of the mins. use x values like in bar-ip -> joint
     # Reading in the files
     k_b = 0.0019872041
     T = 300 
@@ -27,6 +28,7 @@ def bar_ip(delta_U1_filename, delta_U2_filename):
     delta_U_mesh_plot = np.linspace(delta_U_min-mesh_size/2, delta_U_max+mesh_size/2, plotMeshCount)
     hist_U1_values = np.histogram(delta_U1_values, delta_U_mesh_edges, normed=True)[0]
     hist_U2_values = np.histogram(delta_U2_values, delta_U_mesh_edges, normed=True)[0]
+    # The problem with using automatic bin size determination is that the regions will be different for U1, U2 -> problems plotting? and we want the IP also to consider the space in between, right? Or not?
 
     # Fitting the distribution to the data
     ## Model functions
@@ -52,8 +54,9 @@ def bar_ip(delta_U1_filename, delta_U2_filename):
         value = np.exp(model_fct_paras[0] * np.cos(x)) / (2*np.pi*scipy.special.iv(0,model_fct_paras[0]))
         print value
         return value
-    
-    # Residual function - fitting p1 directly 
+
+
+    # Residual function - fitting p1 directly
     def residual_fct_1(paras):
         residues = np.zeros(len(delta_U_mesh_centers) * 2)
         # Residuals of the first distribution
@@ -117,15 +120,16 @@ def bar_ip(delta_U1_filename, delta_U2_filename):
         print model_fct_U2_values
         print "residues"
         print residues
+
         # Returning the residuals
         return residues
 
     # Least squares fitting
-    #paras_initial = np.array([0, 10, 0]) # normal distribution
-    paras_initial = np.array([0, 10, 0, 10, 0]) # normal distribution_2
+    paras_initial = np.array([0, 10, 0]) # normal distribution, first value is the delta_G
+    #paras_initial = np.array([0, 10, 0, 10, 0]) # normal distribution_2
     #paras_initial = np.array([0, 1]) # von mises 
-    model_fct = dist_normal_2
-    paras_final, success = leastsq(residual_fct_1, paras_initial[:], ftol=1.49012e-8, xtol=1.49012e-8)  # Switching function
+    model_fct = dist_normal
+    paras_final, success = leastsq(residual_fct_2, paras_initial[:], ftol=1.49012e-8, xtol=1.49012e-8)  # Switching function
         
     # Printing the results of the LS fit
     print "Estimated parameteres"
@@ -134,20 +138,28 @@ def bar_ip(delta_U1_filename, delta_U2_filename):
     print "Model function parameters: ", paras_final[1:]    
     
     # Plotting the resulting fit
-    model_fct_U1_values_fit = model_fct(delta_U_mesh_centers, paras_final[1:]) # Switchting this two with the next two
-    model_fct_U2_values_fit = np.exp((-(k_b*T)**(-1))*paras_final[0] - delta_U_mesh_centers) * model_fct_U1_values_fit
+    # residual_fct_1
+    #model_fct_U1_values_fit = model_fct(delta_U_mesh_centers, paras_final[1:]) # Switchting this two with the next two
+    #model_fct_U2_values_fit = np.exp((-(k_b*T)**(-1))*paras_final[0] - delta_U_mesh_centers) * model_fct_U1_values_fit
+    # residual_fct_2
+    model_fct_U2_values_fit = model_fct(delta_U_mesh_centers, paras_final[1:])
+    model_fct_U1_values_fit = model_fct_U2_values_fit / np.exp((-(k_b*T)**(-1))*paras_final[0] - delta_U_mesh_centers)
     
-    #model_fct_U2_values_fit = model_fct(delta_U_mesh_centers, paras_final[1:])
-    #model_fct_U1_values_fit = model_fct_U2_values_fit / np.exp((-(k_b*T)**(-1))*paras_final[0] - delta_U_mesh_centers)
-    
-    plt.plot(delta_U_mesh_centers,model_fct_U1_values_fit,'m-')
+    plt.plot(delta_U_mesh_centers, model_fct_U1_values_fit,'m-')
     plt.plot(delta_U_mesh_centers, hist_U1_values, 'ko')
     # plt.plot(delta_U_mesh_centers,hist_U1_values,'bo', delta_U_mesh_plot,model_fct_U1_values_fit,'b-', delta_U_mesh_centers,hist_U2_values,'ko', delta_U_mesh_plot,model_fct_U2_values_fit,'m-')
-    plt.plot(delta_U_mesh_centers,hist_U2_values,'bo')
+    plt.plot(delta_U_mesh_centers, hist_U2_values,'bo')
     plt.plot(delta_U_mesh_centers, model_fct_U2_values_fit, 'c-')
+    plt.xlabel('DeltaU')
+    plt.ylabel('p(Delta U)')
+    plt.xticks(np.arange(min(delta_U_mesh_centers), max(delta_U_mesh_centers) + 1, 1.0))
 
+    #plt.savefig("plot.unreduced.png", bbox_inches='tight')
+    #plt.savefig("plot.unreduced.pdf", bbox_inches='tight')
     plt.show()
     
+
+
 
 def help():
     print "Usage: hqf_fec_run_bar_ip.py <file with U1_U2-U1_U1 values> <file with U2_U2-U2_U1 values>"

hyperq/bar.py
@@ -30,7 +30,7 @@ from hyperq.functions import *
 class BAR:
     
     def __init__(self, U1_U1_filename, U1_U2_filename, U2_U1_filename, U2_U2_filename, C_filename, temp, outputFilename):
-        # The first potential is always the sampling potential, the second one is the evaluating potential.\n\n
+        # The first potential is always the sampling potential, the second one is the evaluating potential.
 
         # Reading in the values of the input files into lists
         filenames = [U1_U1_filename, U1_U2_filename, U2_U1_filename, U2_U2_filename, C_filename]
@@ -47,13 +47,14 @@ class BAR:
         self.C_filename = C_filename
         self.C_values_accepted = [] # We only need the C-values for which the sum2 is not zero, since we are skipping such ones
         self.delta_F_1 = []
-        self.delta_F_1_reduced = []
+        #self.delta_F_1_reduced = []
         self.delta_F_2 = []
-        self.delta_F_2_reduced = []
+        #self.delta_F_2_reduced = []
         self.temp = float(temp)  # Kelvin
-        # self.energyFactor = 627.509469           # hartree -> kcal/mole    # https://en.wikipedia.org/wiki/Hartree
+        # self.energyFactor = 627.509469           # hartree -> kcal/mol    # https://en.wikipedia.org/wiki/Hartree
         self.energyFactor = 1
         self.k_b = 0.0019872041  # in kcal/(mol*K)
+        self.beta = 1./(self.k_b * self.temp)
         self.plt = None
         self.outputFilename = outputFilename
     
@@ -67,8 +68,6 @@ class BAR:
             # First BAR equation
             sum_1 = 0
             sum_2 = 0
-            n_1 = 0
-            n_2 = 0
             # Checking if the number of values provided are the same for each pair
             if len(self.values[self.U1_U1_filename]) == len(self.values[self.U1_U2_filename]):
                 n_1 = len(self.values[self.U1_U1_filename])  # The number of snapshots from the sampling wrt to U1 -> coordinates of U1 MD
@@ -80,29 +79,29 @@ class BAR:
             else:
                 errorMessage="Error: The files " + self.U2_U1_filename + " and " + self.U2_U2_filename + " contain an unequal number of values."
                 raise TypeError(errorMessage)
-            for i in range(0, n_2 - 1):
-                sum_2 += fermi_function((self.k_b * self.temp) ** (-1) * self.energyFactor * (self.values[self.U2_U1_filename][i] - self.values[self.U2_U2_filename][i]) + C)
-            for i in range(0, n_1 - 1):
-                sum_1 += fermi_function((self.k_b * self.temp) ** (-1) * self.energyFactor * (self.values[self.U1_U2_filename][i] - self.values[self.U1_U1_filename][i]) - C)
+            for i in range(0, n_2):
+                sum_2 += fermi_function(self.beta * self.energyFactor * (self.values[self.U2_U1_filename][i] - self.values[self.U2_U2_filename][i]) + C)
+            for i in range(0, n_1):
+                sum_1 += fermi_function(self.beta * self.energyFactor * (self.values[self.U1_U2_filename][i] - self.values[self.U1_U1_filename][i]) - C)
 
             # If sum_2 is 0 we cannot use it... -> math error
             if sum_2 == 0:
                 print("Warning: sum_2 is zero. Skipping this C-value...")
                 continue
 
-            self.delta_F_1_reduced.append(math.log(sum_2 / sum_1) + C - math.log(n_2 / n_1))
-            self.delta_F_1.append(-(self.k_b * self.temp) * (math.log(sum_2 / sum_1) + C - math.log(n_2 / n_1)))  # https://en.wikipedia.org/wiki/Boltzmann_constant
+            #self.delta_F_1_reduced.append(math.log(sum_2 / sum_1) + C - math.log(n_2 / n_1))
+            self.delta_F_1.append((self.k_b * self.temp) * (math.log(sum_2 / sum_1) + C - math.log(n_2 / n_1)))  # https://en.wikipedia.org/wiki/Boltzmann_constant
             self.C_values_accepted.append(C)
 
             # Second BAR equation
-            self.delta_F_2_reduced.append(C - math.log(n_2 / n_1))
-            self.delta_F_2.append(-(self.k_b * self.temp) * (C - math.log(n_2 / n_1)))
+            # DeltaF= -k_b*T*(C - math.log(n_2 / n_1))
+            self.delta_F_2.append((self.k_b * self.temp) * (C - math.log(n_2 / n_1)))
     
             # Printing some information
-            print "delta_F_1_reduced (eqn_1) =", self.delta_F_1_reduced[-1]
-            print "delta_F_2_reduced (eqn_2) =", self.delta_F_2_reduced[-1]
-            print "delta_F_1 (eqn_1) =", self.delta_F_1_reduced[-1]
-            print "delta_F_2 (eqn_2) =", self.delta_F_2_reduced[-1]
+            #print "delta_F_1_reduced (eqn_1) =", self.delta_F_1_reduced[-1]
+            #print "delta_F_2_reduced (eqn_2) =", self.delta_F_2_reduced[-1]
+            print "delta_F_1 (eqn_1) =", self.delta_F_1[-1]
+            print "delta_F_2 (eqn_2) =", self.delta_F_2[-1]
 
     
         # Finding the intersection/least distance
@@ -113,31 +112,31 @@ class BAR:
 
         self.diff_F_min_1 = self.delta_F_1[self.diff_F_min_index]
         self.diff_F_min_2 = self.delta_F_2[self.diff_F_min_index]
-        self.diff_F_min_1_reduced = self.delta_F_1_reduced[self.diff_F_min_index]
-        self.diff_F_min_2_reduced = self.delta_F_2_reduced[self.diff_F_min_index]
+        #self.diff_F_min_1_reduced = self.delta_F_1_reduced[self.diff_F_min_index]
+        #self.diff_F_min_2_reduced = self.delta_F_2_reduced[self.diff_F_min_index]
         print
         print "                    Final results"
         print "****************************************************************"
         print "Min (Delta F_eqn1 - F_eqn2) at value C=", self.C_values_accepted[self.diff_F_min_index]
         print "Delta_F equation 1: ", self.diff_F_min_1, " kcal/mol"
         print "Delta_F equation 2: ", self.diff_F_min_2, " kcal/mol"
-        print "Delta_F equation 1 reduced: ", self.diff_F_min_1_reduced
-        print "Delta_F equation 2 reduced: ", self.diff_F_min_2_reduced
+        #print "Delta_F equation 1 reduced: ", self.diff_F_min_1_reduced
+        #print "Delta_F equation 2 reduced: ", self.diff_F_min_2_reduced
     
     
     def write_delta_F_values(self):
         # Writing out the values
-        with open("bar.out.eqn_1_reduced", "w") as file:
-            for i in range(0, len(self.C_values_accepted) - 1):
-                file.write(str(self.delta_F_1_reduced[i]) + "\n")
+        #with open("bar.out.eqn_1_reduced", "w") as file:
+        #    for i in range(0, len(self.C_values_accepted) - 1):
+        #        file.write(str(self.delta_F_1_reduced[i]) + "\n")
 
         with open("bar.out.eqn_1", "w") as file:
             for i in range(0, len(self.C_values_accepted) - 1):
                 file.write(str(self.delta_F_1[i]) + "\n")
 
-        with open("bar.out.eqn_2_reduced", "w") as file:
-            for i in range(0, len(self.C_values_accepted) - 1):
-                file.write(str(self.delta_F_2_reduced[i]) + "\n")
+        #with open("bar.out.eqn_2_reduced", "w") as file:
+        #    for i in range(0, len(self.C_values_accepted) - 1):
+        #        file.write(str(self.delta_F_2_reduced[i]) + "\n")
 
         with open("bar.out.eqn_2", "w") as file:
             for i in range(0, len(self.C_values_accepted) - 1):
@@ -148,11 +147,11 @@ class BAR:
 
         with open(self.outputFilename+".values", "w") as file:
             
-            file.write("Min (Delta F_eqn1 - F_eqn2) at value C=" + str(self.C_values_accepted[self.diff_F_min_index]) + "\n")
+            file.write("Min (Delta F_eqn1 - F_eqn2) found at location C=" + str(self.C_values_accepted[self.diff_F_min_index]) + "\n")
             file.write("Delta_F equation 1: " + str(self.diff_F_min_1) + " kcal/mol" + "\n")
             file.write("Delta_F equation 2: "+ str(self.diff_F_min_2) + " kcal/mol" + "\n")
-            file.write("Delta_F equation 1 reduced: " + str(self.diff_F_min_1_reduced) + "\n")
-            file.write("Delta_F equation 2 reduced: " + str(self.diff_F_min_2_reduced) + "\n")
+            #file.write("Delta_F equation 1 reduced: " + str(self.diff_F_min_1_reduced) + "\n")
+            #file.write("Delta_F equation 2 reduced: " + str(self.diff_F_min_2_reduced) + "\n")
 
     
     def plot(self, mode):
@@ -175,11 +174,146 @@ class BAR:
             plt.savefig(self.outputFilename +".plot.unreduced.png", bbox_inches='tight')
             plt.savefig(self.outputFilename +".plot.unreduced.pdf", bbox_inches='tight')
 
-        # Creating the plot for the reduced equation
-        plt.clf()
+        # # Creating the plot for the reduced equation
+        # plt.clf()
+        # print "\n\n Creating the plot related to the bar-method"
+        # plt.plot(self.C_values_accepted, self.delta_F_1_reduced, 'm--', label="Equation 1")
+        # plt.plot(self.C_values_accepted, self.delta_F_2_reduced, 'k--', label="Equation 2")
+        # plt.xlabel('C')
+        # plt.ylabel('Free energy difference [kcal/mole]')
+        #
+        # # Saving/displaying the plot
+        # # Commented out because we set plt.switch_backend('agg'), which does not support showing plots
+        # if mode == "plot":
+        #     plt.switch_backend('TkAgg')
+        #     plt.show()
+        #
+        # # Showing the plot
+        # elif mode == "save":
+        #     plt.savefig(self.outputFilename +".plot.reduced.png", bbox_inches='tight')
+        #     plt.savefig(self.outputFilename +".plot.reduced.pdf", bbox_inches='tight')
+
+
+class BAR_DeltaU:
+    def __init__(self, U1_U2_minus_U1_U1_filename, U2_U1_minus_U2_U2_filename, C_filename, temp,
+                 outputFilename):
+        # The first potential is always the sampling potential, the second one is the evaluating potential.
+
+        # Reading in the values of the input files into lists
+        filenames = [U1_U2_minus_U1_U1_filename, U2_U1_minus_U2_U2_filename, C_filename]
+        # value_types = ["U1_U1_values", "U1_U2_values", "U2_U1_values", "U2_U2_values", "C_values"]
+        self.values = dict.fromkeys(filenames)
+        for filename in filenames:
+            self.values[filename] = np.loadtxt(filename)
+
+        # Variables
+        self.U1_U2_minus_U1_U1_filename = U1_U2_minus_U1_U1_filename
+        self.U2_U1_minus_U2_U2_filename = U2_U1_minus_U2_U2_filename
+        self.C_filename = C_filename
+        self.C_values_accepted = []  # We only need the C-values for which the sum2 is not zero, since we are skipping such ones
+        self.delta_F_1 = []
+        # self.delta_F_1_reduced = []
+        self.delta_F_2 = []
+        # self.delta_F_2_reduced = []
+        self.temp = float(temp)  # Kelvin
+        # self.energyFactor = 627.509469           # hartree -> kcal/mol    # https://en.wikipedia.org/wiki/Hartree
+        self.energyFactor = 1
+        self.k_b = 0.0019872041  # in kcal/(mol*K)
+        self.beta = 1. / (self.k_b * self.temp)
+        self.plt = None
+        self.outputFilename = outputFilename
+
+    def compute_bar(self):
+        # Loop for each C-value
+        for C in self.values[self.C_filename]:
+
+            # Printing some information
+            print "=======================    C=%07.3f   =======================" % C
+
+            # First BAR equation
+            sum_1 = 0
+            sum_2 = 0
+            n_1 = len(self.values[self.U1_U2_minus_U1_U1_filename])
+            n_2 = len(self.values[self.U2_U1_minus_U2_U2_filename])  # The number of snapshots from the sampling wrt to U2 -> coordinates of U2 MD
+
+            for i in range(0, n_2):
+                sum_2 += fermi_function(self.beta * self.energyFactor * (self.values[self.U2_U1_minus_U2_U2_filename][i]) + C)
+            for i in range(0, n_1):
+                sum_1 += fermi_function(self.beta * self.energyFactor * (self.values[self.U1_U2_minus_U1_U1_filename][i]) - C)
+
+            # If sum_2 is 0 we cannot use it... -> math error
+            if sum_2 == 0:
+                print("Warning: sum_2 is zero. Skipping this C-value...")
+                continue
+
+            # self.delta_F_1_reduced.append(math.log(sum_2 / sum_1) + C - math.log(n_2 / n_1))
+            self.delta_F_1.append((self.k_b * self.temp) * (
+            math.log(sum_2 / sum_1) + C - math.log(n_2 / n_1)))  # https://en.wikipedia.org/wiki/Boltzmann_constant
+            self.C_values_accepted.append(C)
+
+            # Second BAR equation
+            # self.delta_F_2_reduced.append(C - math.log(n_2 / n_1))
+            self.delta_F_2.append((self.k_b * self.temp) * (C - math.log(n_2 / n_1)))
+
+            # Printing some information
+            # print "delta_F_1_reduced (eqn_1) =", self.delta_F_1_reduced[-1]
+            # print "delta_F_2_reduced (eqn_2) =", self.delta_F_2_reduced[-1]
+            print "delta_F_1 (eqn_1) =", self.delta_F_1[-1]
+            print "delta_F_2 (eqn_2) =", self.delta_F_2[-1]
+
+        # Finding the intersection/least distance
+        self.diff_F = np.absolute(np.asarray(self.delta_F_2) - np.asarray(self.delta_F_1))
+        self.diff_F_min_index = np.argmin(self.diff_F)
+        if isinstance(self.diff_F_min_index, np.ndarray):
+            self.diff_F_min_index = self.diff_F_min_index[0]
+
+        self.diff_F_min_1 = self.delta_F_1[self.diff_F_min_index]
+        self.diff_F_min_2 = self.delta_F_2[self.diff_F_min_index]
+        # self.diff_F_min_1_reduced = self.delta_F_1_reduced[self.diff_F_min_index]
+        # self.diff_F_min_2_reduced = self.delta_F_2_reduced[self.diff_F_min_index]
+        print
+        print "                    Final results"
+        print "****************************************************************"
+        print "Min (Delta F_eqn1 - F_eqn2) at value C=", self.C_values_accepted[self.diff_F_min_index]
+        print "Delta_F equation 1: ", self.diff_F_min_1, " kcal/mol"
+        print "Delta_F equation 2: ", self.diff_F_min_2, " kcal/mol"
+        # print "Delta_F equation 1 reduced: ", self.diff_F_min_1_reduced
+        # print "Delta_F equation 2 reduced: ", self.diff_F_min_2_reduced
+
+    def write_delta_F_values(self):
+        # Writing out the values
+        # with open("bar.out.eqn_1_reduced", "w") as file:
+        #    for i in range(0, len(self.C_values_accepted) - 1):
+        #        file.write(str(self.delta_F_1_reduced[i]) + "\n")
+
+        with open("bar.out.eqn_1", "w") as file:
+            for i in range(0, len(self.C_values_accepted) - 1):
+                file.write(str(self.delta_F_1[i]) + "\n")
+
+        # with open("bar.out.eqn_2_reduced", "w") as file:
+        #    for i in range(0, len(self.C_values_accepted) - 1):
+        #        file.write(str(self.delta_F_2_reduced[i]) + "\n")
+
+        with open("bar.out.eqn_2", "w") as file:
+            for i in range(0, len(self.C_values_accepted) - 1):
+                file.write(str(self.delta_F_2[i]) + "\n")
+
+    def write_results(self):
+
+        with open(self.outputFilename + ".values", "w") as file:
+            file.write("Min (Delta F_eqn1 - F_eqn2) found at location C=" + str(
+                self.C_values_accepted[self.diff_F_min_index]) + "\n")
+            file.write("Delta_F equation 1: " + str(self.diff_F_min_1) + " kcal/mol" + "\n")
+            file.write("Delta_F equation 2: " + str(self.diff_F_min_2) + " kcal/mol" + "\n")
+            # file.write("Delta_F equation 1 reduced: " + str(self.diff_F_min_1_reduced) + "\n")
+            # file.write("Delta_F equation 2 reduced: " + str(self.diff_F_min_2_reduced) + "\n")
+
+    def plot(self, mode):
+
+        # Creating the plot for the full (unreduced) equation
         print "\n\n Creating the plot related to the bar-method"
-        plt.plot(self.C_values_accepted, self.delta_F_1_reduced, 'm--', label="Equation 1")
-        plt.plot(self.C_values_accepted, self.delta_F_2_reduced, 'k--', label="Equation 2")
+        plt.plot(self.C_values_accepted, self.delta_F_1, 'm--', label="Equation 1")
+        plt.plot(self.C_values_accepted, self.delta_F_2, 'k--', label="Equation 2")
         plt.xlabel('C')
         plt.ylabel('Free energy difference [kcal/mole]')
 
@@ -191,5 +325,24 @@ class BAR:
 
         # Showing the plot
         elif mode == "save":
-            plt.savefig(self.outputFilename +".plot.reduced.png", bbox_inches='tight')
-            plt.savefig(self.outputFilename +".plot.reduced.pdf", bbox_inches='tight')
+            plt.savefig(self.outputFilename + ".plot.unreduced.png", bbox_inches='tight')
+            plt.savefig(self.outputFilename + ".plot.unreduced.pdf", bbox_inches='tight')
+
+            # # Creating the plot for the reduced equation
+            # plt.clf()
+            # print "\n\n Creating the plot related to the bar-method"
+            # plt.plot(self.C_values_accepted, self.delta_F_1_reduced, 'm--', label="Equation 1")
+            # plt.plot(self.C_values_accepted, self.delta_F_2_reduced, 'k--', label="Equation 2")
+            # plt.xlabel('C')
+            # plt.ylabel('Free energy difference [kcal/mole]')
+            #
+            # # Saving/displaying the plot
+            # # Commented out because we set plt.switch_backend('agg'), which does not support showing plots
+            # if mode == "plot":
+            #     plt.switch_backend('TkAgg')
+            #     plt.show()
+            #
+            # # Showing the plot
+            # elif mode == "save":
+            #     plt.savefig(self.outputFilename +".plot.reduced.png", bbox_inches='tight')
+            #     plt.savefig(self.outputFilename +".plot.reduced.pdf", bbox_inches='tight')